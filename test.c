2d43489ef80084630641efdf0d2dcd6bdeb07e18
Ayfred <maxime.mu@mines-ales.org>
2023-03-18 09:50:35 +0100

    v1.0 : code functional

--- test.c
+++ test.c
@@ -8,8 +8,20 @@
 
 
 int board[NbRows][NbColumns] = {0};
-int sum = 0;
+int total = 0;
 int size;
+int rotation = 4;
+/*
+bool matrixHasNoZero(int matrix[NbRows][NbColumns]) {
+    for (int i = 0; i < NbRows; i++) {
+        for (int j = 0; j < NbColumns; j++) {
+            if (matrix[i][j] == 0) {
+                return false;
+            }
+        }
+    }
+    return true;
+}*/
 
 
 int pieces[NbPieces][MaxRotations][5][2] = {
@@ -24,7 +36,7 @@ int pieces[NbPieces][MaxRotations][5][2] = {
     {{{0,0}, {0,1}, {0,2}, {1,0}, {2,0}}, {{0,0}, {0,1}, {0,2}, {1, 2}, {2,2}}, {{0, 2}, {1, 2}, {2, 0}, {2,1}, {2,2}}, {{0, 0}, {1, 0}, {2, 0}, {2,1}, {2,2}}},
 
     //Le Z (symétrie)
-    {{{0,0}, {0,1}, {1,1}, {2, 1}, {2,2}}, {{0, 2}, {1, 0}, {1, 1}, {1, 2}, {2,0}}, {{0,0}, {0,1}, {1,1}, {2, 1}, {2,2}}, {{0, 2}, {1, 0}, {1, 1}, {1, 2}, {2,0}}},
+    {{{0,0}, {0,1}, {1,1}, {2, 1}, {2,2}}, {{0, 2}, {1, 0}, {1, 1}, {1, 2}, {2,0}}},
 
     //Le long Z
     {{{0,1}, {1,1}, {2,0}, {2, 1}, {3,0}}, {{0, 0}, {0, 1}, {1, 1}, {1, 2}, {1,3}}, {{0,1}, {1,0}, {1,1}, {2, 0}, {3,0}}, {{0,0}, {0,1}, {0, 2}, {1, 2}, {1,3}}},
@@ -39,10 +51,10 @@ int pieces[NbPieces][MaxRotations][5][2] = {
     {{{0,1}, {1,1}, {2,0}, {2,1}}, {{0,0}, {1,0}, {1,1}, {1,2}}, {{0,0}, {0,1}, {1,0}, {2,0}}, {{0,0}, {0,1}, {0,2}, {1,2}}},
 
     //Le Z inversé debout
-    {{{0,0}, {1,0}, {1,1}, {2,1}}, {{0,1}, {0,2}, {1,0}, {1,1}}, {{0,0}, {1,0}, {1,1}, {2,1}}, {{0,1}, {0,2}, {1,0}, {1,1}}},
+    {{{0,0}, {1,0}, {1,1}, {2,1}}, {{0,1}, {0,2}, {1,0}, {1,1}}},
 
     //Le I renversé
-    {{{0,0}, {0,1}, {0,2}, {0,3}}, {{0,0}, {1,0}, {2,0}, {3,0}},{{0,0}, {0,1}, {0,2}, {0,3}}, {{0,0}, {1,0}, {2,0}, {3,0}}},
+    {{{0,0}, {0,1}, {0,2}, {0,3}}, {{0,0}, {1,0}, {2,0}, {3,0}}},
 
     //Le L
     //{{{0,0}, {1,0}, {2,0}, {2,1}}, {{0,0}, {0,1}, {0,2}, {1,0}}, {{0,0}, {0,1}, {1,1}, {1,2}}, {{0,2}, {1,0}, {1,1}, {1,2}}},
@@ -58,47 +70,69 @@ bool PlacePiece(int piece_num, int rotation_num, int x, int y) {
     if(piece_num <=6)
     {
         size = 5;
+        if(piece_num == 3){
+            rotation = 2;
+        }
+        else{
+            rotation = 4;
+        }
     }
     else{
         size = 4;
+        if(piece_num >= 8){
+            rotation = 2;
+        }else{
+        rotation = 4;
+        }
     }
 
-    // place the piece on the board if it fits
+
     for (int i = 0; i < size; i++) {
         int piece_x = pieces[piece_num][rotation_num][i][0];
         int piece_y = pieces[piece_num][rotation_num][i][1];
         if (x + piece_x < 0 || x + piece_x >= NbRows || y + piece_y < 0 || y + piece_y >= NbColumns) {
-            return false; // piece is out of bounds
+            return false;
         }
         if (board[x + piece_x][y + piece_y] != 0) {
-            return false; // piece overlaps with another piece
+            return false;
         }
     }
 
-    // place the piece on the board
+
     for (int i = 0; i < size; i++) {
         //printf("%i\n", size);
         int piece_x = pieces[piece_num][rotation_num][i][0];
         int piece_y = pieces[piece_num][rotation_num][i][1];
-        board[x + piece_x][y + piece_y] = piece_num + 1; // mark the square as occupied by the piece (use piece_num + 1 to avoid using 0 as a valid piece number)
-        }
-    return true; // successfully placed the piece
+        board[x + piece_x][y + piece_y] = piece_num + 1;}
+    return true;
+
 }
 
 void remove_piece(int piece_num, int rotation_num, int x, int y) {
     if(piece_num <=6)
     {
         size = 5;
+        if(piece_num == 3){
+            rotation = 2;
+        }
+        else{
+            rotation = 4;
+        }
     }
     else{
         size = 4;
+        if(piece_num >= 8){
+            rotation = 2;
+        }else{
+        rotation = 4;
+        }
     }
     //printf("piece_num = %d, size = %d, rotation_num = %d, x = %d, y= %d\n", piece_num, size, rotation_num, x, y);
-    // remove the piece from the board
+
     for (int i = 0; i < size; i++) {
         int piece_x = pieces[piece_num][rotation_num][i][0];
         int piece_y = pieces[piece_num][rotation_num][i][1];
-        board[x + piece_x][y + piece_y] = 0; // mark the square as unoccupied
+        board[x + piece_x][y + piece_y] = 0;
     }
 }
 
@@ -106,10 +140,26 @@ void remove_piece(int piece_num, int rotation_num, int x, int y) {
 void solve_tetro_puzzle(int piece_num, int number) {
     int r, i ,j;
 
+    if(piece_num <=6)
+    {
+        if(piece_num == 3){
+            rotation = 2;
+        }
+        else{
+            rotation = 4;
+        }
+    }
+    else{
+        if(piece_num >= 8){
+            rotation = 2;
+        }else{
+        rotation = 4;
+        }
+    }
+
     //printf("Placing piece current number : %i\n", piece_num);
 
-    // recursively try to place each piece on the board
-    for(r = 0; r < MaxRotations; r++){
+    for(r = 0; r < rotation; r++){
         for(i = 0; i < NbRows; i++){
             for(j = 0; j < NbColumns; j++){
                 //printf("i = %i, j = %i, r = %i\n", i, j, r);
@@ -128,15 +178,17 @@ void solve_tetro_puzzle(int piece_num, int number) {
                     if (piece_num == number)
                     {
 
-                        sum += 1;
-                        // all pieces have been successfully placed on the board
-                        printf("%s", "Solution found !\n");
+                        total += 1;
+                        /*printf("Solution found (%d)!\n", total);
 
+                        for (int x = 0; x < NbRows; x++) {
+                            for (int y = 0; y < NbColumns; y++) {
+                            printf("%d ", board[x][y]);
+                            }
+                            printf("\n");
+                        }
+                        printf("\n");*/
 
-                        solve_tetro_puzzle(piece_num + 1, number);
-
-                        //remove_piece(piece_num, r, i, j);
-                        continue;
                     }
                     else
                     {
@@ -145,12 +197,6 @@ void solve_tetro_puzzle(int piece_num, int number) {
                     }
                     //printf("Removing piece number %i\n", piece_num+1);
                     remove_piece(piece_num, r, i, j);
-                    /*for (int y = 0; y < NbRows; y++) {
-                            for (int x = 0; x < NbColumns; x++) {
-                                printf("%d ", board[y][x]);
-                            }
-                            printf("\n");
-                            }*/
                 }
             }
         }
@@ -160,11 +206,19 @@ void solve_tetro_puzzle(int piece_num, int number) {
 
 
 
-int main() {
-
+int main(){
+    board[0][6] = 9;
+    board[1][6] = 9;
+    board[7][0] = 9;
+    board[7][1] = 9;
+    board[7][2] = 9;
+    board[7][3] = 9;
+    board[0][0] = 9;
+    board[2][0] = 9;
+    //board[6][3] = 9;
+    board[7][6] = 9;
     solve_tetro_puzzle(0, NbPieces-1);
-    printf("sum = %i\n", sum);
+    printf("total = %i\n", total);
 
     return 0;
 }
-
